c132ac43e3d532fb8b49e005713652d281907297
ana-konzen <143814540+ana-konzen@users.noreply.github.com>
2025-02-27 20:38:07 -0500

    add callout and quote components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,7 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
         red: "var(--red)",
+        lightRed: "var(--light-red)",
         cream: "var(--cream)",
         black: "var(--black)",
         white: "var(--white)",
