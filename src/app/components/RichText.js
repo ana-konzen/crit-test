828fffd3d3c9b48c61c27c9fea8d1aa8cd6d524c
ana-konzen <143814540+ana-konzen@users.noreply.github.com>
2025-02-27 20:38:39 -0500

    add richText component

--- src/app/components/RichText.js
+++ src/app/components/RichText.js
@@ -8,9 +8,16 @@ export default function RichText({ richText }) {
           "font-bold": item.annotations.bold,
           "font-semibold text-red mr-2": item.annotations.code,
           italic: item.annotations.italic,
-          underline: item.annotations.underline,
+          underline: item.annotations.underline || item.href,
           hidden: item.annotations.color === "gray",
         });
+        if (item.href) {
+          return (
+            <a key={index} href={item.href} className={itemClass} target="_blank">
+              {item.plain_text}
+            </a>
+          );
+        }
         return (
           <span key={index} className={itemClass}>
             {item.plain_text}
