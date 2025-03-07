f63ff7e5599c069c90ccf5f98f3bb7f09292bbd6
ana-konzen <143814540+ana-konzen@users.noreply.github.com>
2025-03-07 01:37:40 -0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import { getPageContent } from "@/notion/notion";
 import PageLayout from "@/app/layout/PageLayout";
 
 export default async function Page() {
-  const pageContent = await getPageContent("1a15ae7ea4ba804385b8d1048e3bc3d4");
+  const pageContent = await getPageContent({ block_id: "1a15ae7ea4ba804385b8d1048e3bc3d4" });
 
   return <PageLayout pageContent={pageContent} />;
 }
