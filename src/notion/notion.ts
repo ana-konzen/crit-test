ed287ce55d6b6d41787f526637274955818d1a81
ana-konzen <143814540+ana-konzen@users.noreply.github.com>
2025-03-20 20:47:01 -0400

    refactor getToc to return a structured array, remove unused types

--- src/notion/notion.ts
+++ src/notion/notion.ts
@@ -9,15 +9,14 @@ import type {
   PartialPageObjectResponse,
   PageObjectResponse,
   BlockObjectResponse,
-  LinkToPageBlockObjectResponse,
   PartialBlockObjectResponse,
 } from "@notionhq/client/build/src/api-endpoints";
 
 import { createSlug } from "@/util";
 
 type Id = string;
-// todo: remove slug when no longer needed
-type CustomBlock = BlockObjectResponse & { children?: BlockObjectResponse[]; slug?: string };
+
+type CustomBlock = BlockObjectResponse & { children?: BlockObjectResponse[] };
 
 function isFullPage(
   response: PageObjectResponse | PartialPageObjectResponse
@@ -65,63 +64,63 @@ async function fetchPageTitle({ page_id }: GetPageParameters) {
   return response.properties.title.title[0].plain_text.trim();
 }
 
-// todo: have getToc return a simple data structure instead of the notion stuff
-interface ExtendedLinkToPageBlockObjectResponse extends LinkToPageBlockObjectResponse {
+interface TocItem {
+  title: string;
+  slug: string;
+  id: string;
+  children: TocChild[];
+}
+
+interface TocChild {
   page_id: string;
   title: string;
   slug: string;
 }
-export async function getToc(): Promise<CustomBlock[]> {
+
+export async function getToc(): Promise<TocItem[]> {
   const blocks = await fetchBlockChildren("1a75ae7ea4ba8030a2dcc88dafa1b27a");
+  const toc = [];
+  for await (const block of blocks) {
+    if (!block.has_children) continue;
+    const blockChildren = await fetchBlockChildren(block.id);
+    const extendedChildren = [];
+    for await (const child of blockChildren) {
+      if (child.type !== "link_to_page" || child.link_to_page.type !== "page_id") continue;
+      const childTitle = await fetchPageTitle({ page_id: child.link_to_page.page_id });
 
-  await Promise.all(
-    blocks
-      .filter((block) => block.type === "toggle" && block.has_children)
-      .map(async (block: CustomBlock) => {
-        const blockChildren = await fetchBlockChildren(block.id);
+      extendedChildren.push({
+        page_id: child.link_to_page.page_id,
+        title: childTitle,
+        slug: createSlug(childTitle),
+      });
+    }
+    if (block.type === "toggle") {
+      toc.push({
+        title: block.toggle.rich_text[0].plain_text,
+        slug: createSlug(block.toggle.rich_text[0].plain_text),
+        id: block.id,
+        children: extendedChildren,
+      });
+    }
+  }
+  console.log(toc);
 
-        await Promise.all(
-          blockChildren
-            .filter((child) => child.type === "link_to_page")
-            .map(async (child) => {
-              const extendedChild = child as ExtendedLinkToPageBlockObjectResponse;
-
-              if (!(child.link_to_page.type === "page_id")) {
-                throw new Error("Expected link_to_page to be of type page_id");
-              }
-              extendedChild.page_id = child.link_to_page.page_id;
-              extendedChild.title = (await fetchPageTitle({ page_id: extendedChild.page_id })) ?? "";
-              extendedChild.slug = createSlug(extendedChild.title);
-            })
-        );
-
-        if (block.type === "toggle") {
-          block.slug = createSlug(block.toggle.rich_text[0].plain_text);
-        }
-
-        block.children = blockChildren;
-      })
-  );
-
-  return blocks;
+  return toc;
 }
 
 export async function getContentParams() {
   const toc = await getToc();
 
   return toc.flatMap((block) =>
-    block.type === "toggle" && block.has_children && block.children
-      ? block.children
-          .filter((child) => child.type === "link_to_page")
-          .map((child) => {
-            const { page_id, title, slug } = child as ExtendedLinkToPageBlockObjectResponse;
-            return {
-              parent_slug: block.slug,
-              page_id,
-              title,
-              slug,
-            };
-          })
+    block.children
+      ? block.children.map((child) => {
+          return {
+            parent_slug: block.slug,
+            page_id: child.page_id,
+            title: child.title,
+            slug: child.slug,
+          };
+        })
       : []
   );
 }
