03b47b81ff317a6ccb9429410d2b7bbd51ef090b
ana-konzen <143814540+ana-konzen@users.noreply.github.com>
2025-03-21 00:37:33 -0400

    fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { getPageContent } from "@/notion/notion";
+import { getPageContent, fetchPageTitle } from "@/notion/notion";
 
 import PageLayout from "@/app/layout/PageLayout";
 
 export default async function Page() {
   const pageContent = await getPageContent("1a15ae7ea4ba804385b8d1048e3bc3d4");
+  const pageTitle = await fetchPageTitle("1a15ae7ea4ba804385b8d1048e3bc3d4");
 
-  return <PageLayout pageContent={pageContent} />;
+  return <PageLayout pageContent={pageContent} pageTitle={pageTitle} />;
 }
